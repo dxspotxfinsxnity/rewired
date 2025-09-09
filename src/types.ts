1d60f61c242065906ef9a45ead1bd6f430aa77b5
dxspotxfinsxnity <139952636+dxspotxfinsxnity@users.noreply.github.com>
2025-09-09 14:54:24 +0700

    create core type definitions

---
+++ src/types.ts
@@ -0,0 +1,59 @@
+import { Attrs } from "./Attrs.js"
+
+/**
+ * A callback that receives a value of type `T` and an "unsubscribe" function.
+ *
+ * The unsubscribe function allows the consumer to stop receiving further
+ * values. This pattern is common in observable or event emitter APIs.
+ */
+export type Callback<T> = (value: T, unsubscribe: () => void) => void
+
+export type CSSValue = IObservable<null | string> | null | string
+
+export type IEach = {
+	render(reference: WeakRef<ParentNode>): Generator<Node | string>
+}
+
+export type IObservable<T = any> = {
+	is(previousValue: T, currentValue: T): boolean
+	subscribe(callback: Callback<T>): () => void
+	value: T
+}
+
+export type ISwitch = {
+	render(): ChildNode
+}
+
+/**
+ * Select only those properties from `T` whose values are assignable to the type
+ * `U`.
+ */
+export type PickBy<T, U> = {
+	[P in keyof T as T[P] extends U ? P : never]: T[P]
+}
+
+export type Props<T extends keyof HTMLElementTagNameMap> = {
+	[P in keyof Attrs[T] as `${P & string}:${keyof HTMLElementEventMap}`]?: IObservable<
+		Attrs[T][P]
+	>
+} & {
+	[P in keyof HTMLElementTagNameMap[T] as P extends `on${infer U}`
+		? U
+		: never]?: HTMLElementTagNameMap[T][P]
+} & {
+	dataset?: Record<
+		string,
+		DOMStringMap[string] | IObservable<DOMStringMap[string]>
+	>
+	style?: {
+		[key: `--${string}`]: CSSValue
+	} & {
+		[P in keyof CSSStyleDeclaration as CSSStyleDeclaration[P] extends
+			| null
+			| string
+			? P
+			: never]?: CSSValue
+	}
+} & Partial<Attrs[T]>
+
+export type TextContent = boolean | null | number | string
